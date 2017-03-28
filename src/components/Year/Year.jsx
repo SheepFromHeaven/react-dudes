2b4a0c6947364a9340dad2f1cb7a57d6008c66f0
Jonas Begemann <jonas.begemann@virtual-identity.com>
2017-03-28 12:15:35 +0200

    added month and year files, dude

---
+++ src/components/Year/Year.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Year.css';
+
+class Year extends Component {
+    render() {
+
+        return (
+            <div className="year">
+
+            </div>
+        );
+    }
+}
+
+export default Year;
