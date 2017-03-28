3e9234cd241539973be6f39953347e0b748db441
SheepFromHeaven <marcwissler@gmail.com>
2017-03-28 15:57:44 +0200

    Add cw, dude

--- src/mockState.js
+++ src/mockState.js
@@ -5,112 +5,128 @@ export default {
 			dayInWeek: 0,
 			dayInMonth: 1,
 			holidayName: "Nikolaus",
-			monthId: 0
+			monthId: 0,
+			calendarWeek: 1
 		},
 		{
 			id: 1,
 			dayInWeek: 1,
 			dayInMonth: 2,
 			holidayName: "Nikolaus",
-			monthId: 0
+			monthId: 0,
+			calendarWeek: 1
 		},
 		{
 			id: 2,
 			dayInWeek: 2,
 			dayInMonth: 3,
 			holidayName: "Nikolaus",
-			monthId: 0
+			monthId: 0,
+			calendarWeek: 1
 		},
 		{
 			id: 3,
 			dayInWeek: 3,
 			dayInMonth: 4,
 			holidayName: "Nikolaus",
-			monthId: 0
+			monthId: 0,
+			calendarWeek: 1
 		},
 		{
 			id: 4,
 			dayInWeek: 4,
 			dayInMonth: 5,
 			holidayName: "Nikolaus",
-			monthId: 0
+			monthId: 0,
+			calendarWeek: 1
 		},
 		{
 			id: 5,
 			dayInWeek: 5,
 			dayInMonth: 6,
 			holidayName: "Nikolaus",
-			monthId: 0
+			monthId: 0,
+			calendarWeek: 1
 		},
 		{
 			id: 6,
 			dayInWeek: 6,
 			dayInMonth: 7,
 			holidayName: "Nikolaus",
-			monthId: 0
+			monthId: 0,
+			calendarWeek: 1
 		},
 		{
 			id: 7,
 			dayInWeek: 0,
 			dayInMonth: 8,
 			holidayName: "Nikolaus",
-			monthId: 0
+			monthId: 0,
+			calendarWeek: 2
 		},
 		{
 			id: 8,
 			dayInWeek: 1,
 			dayInMonth: 9,
 			holidayName: "Nikolaus",
-			monthId: 0
+			monthId: 0,
+			calendarWeek: 2
 		},
 		{
 			id: 9,
 			dayInWeek: 2,
 			dayInMonth: 10,
 			holidayName: "Nikolaus",
-			monthId: 0
+			monthId: 0,
+			calendarWeek: 2
 		},
 		{
 			id: 10,
 			dayInWeek: 3,
 			dayInMonth: 11,
 			holidayName: "Nikolaus",
-			monthId: 0
+			monthId: 0,
+			calendarWeek: 2
 		},
 		{
 			id: 11,
 			dayInWeek: 4,
 			dayInMonth: 12,
 			holidayName: "Nikolaus",
-			monthId: 0
+			monthId: 0,
+			calendarWeek: 2
 		},
 		{
 			id: 12,
 			dayInWeek: 5,
 			dayInMonth: 13,
 			holidayName: "Nikolaus",
-			monthId: 0
+			monthId: 0,
+			calendarWeek: 2
 		},
 		{
 			id: 13,
 			dayInWeek: 6,
 			dayInMonth: 14,
 			holidayName: "Nikolaus",
-			monthId: 0
+			monthId: 0,
+			calendarWeek: 2
 		},
 		{
 			id: 14,
 			dayInWeek: 0,
 			dayInMonth: 15,
 			holidayName: "Nikolaus",
-			monthId: 0
+			monthId: 0,
+			calendarWeek: 3
 		},
 		{
 			id: 15,
 			dayInWeek: 1,
 			dayInMonth: 16,
 			holidayName: "Nikolaus",
-			monthId: 0
+			monthId: 0,
+			calendarWeek: 3
 		}
 	],
 	months: [
