01abd7d90fa6317db348ff3b1612a8b7698c2baf
Alex Benz <benzalexander@gmx.de>
2017-03-28 15:07:21 +0200

    fixed app.test for use with redux, dude

--- src/components/App.test.jsx
+++ src/components/App.test.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import { combineReducers } from 'redux';
+
+let initState = {
+	days: [
+		{
+			id: 0,
+			dayInWeek: 0,
+			dayInMonth: 1,
+			holidayName: "Nikolaus",
+			monthId: 0
+		}
+	],
+	months: [
+		{
+			id: 0,
+			name: "January",
+			yearId: 0
+		}
+	],
+	years: [
+		{
+			id: 0,
+			name: "2017"
+		}
+	]
+};
+
+let store = createStore(combineReducers({}), initState);
 
 describe('App', () => {
 	it('renders without crashing', () => {
     	const div = document.createElement('div');
-    	ReactDOM.render(<App />, div);
+		ReactDOM.render(<Provider store={store}><App /></Provider>, div);
 	});
 });
--- src/containers/FilledDay.jsx
+++ src/containers/FilledDay.jsx
@@ -1,4 +1,3 @@
-
 import { connect } from 'react-redux'
 import Day from '../components/Day/Day'
 
