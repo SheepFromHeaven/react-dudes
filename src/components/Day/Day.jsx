9fcc683818b2b39a1b43f48ec5b6fc4ad20e25be
Jonas Begemann <jonas.begemann@virtual-identity.com>
2017-03-28 11:43:19 +0200

    added some styles, dude

--- src/components/Day/Day.jsx
+++ src/components/Day/Day.jsx
@@ -6,9 +6,9 @@ class Day extends Component {
 
         return (
             <div className={'day day--' + this.props.weekday}>
-                <span class="day__dayInMonth">{this.props.dayInMonth}</span>
-                <span class="day__weekday">{this.props.weekday}</span>
-                <span class="day__calendarWeek">{this.props.calendarWeek}</span>
+                <span className="day__dayInMonth">{this.props.dayInMonth}</span>
+                <span className="day__weekday">{this.props.weekday}</span>
+                <span className="day__calendarWeek">{this.props.calendarWeek}</span>
             </div>
         );
     }
